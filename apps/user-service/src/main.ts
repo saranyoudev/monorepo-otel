85be6fefc775da878d3c1091287119e868d23e8f
saranyoudev <saranyou_p_dev@outlook.com>
2024-07-23 22:35:28 +0700

    setup open telemetry

--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -1,9 +1,11 @@
+require('@app/opentelemetry')('user-service');
 import { NestFactory } from '@nestjs/core';
 import { UserModule } from './user.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(UserModule);
+
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
     options: {
---
+++ libs/opentelemetry/src/index.ts
@@ -0,0 +1,46 @@
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto';
+import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
+import { Resource } from '@opentelemetry/resources';
+import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
+import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
+import { AmqplibInstrumentation } from '@opentelemetry/instrumentation-amqplib';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
+
+function setupOpenTelemetry(serviceName: string) {
+  const sdk = new NodeSDK({
+    resource: new Resource({
+      [SEMRESATTRS_SERVICE_NAME]: serviceName,
+    }),
+    traceExporter: new OTLPTraceExporter({
+      url: 'http://localhost:4318/v1/traces',
+      headers: {},
+    }),
+    metricReader: new PeriodicExportingMetricReader({
+      exporter: new OTLPMetricExporter({
+        url: 'http://localhost:4318/v1/metrics',
+        headers: {},
+      }),
+    }),
+    instrumentations: [
+      new HttpInstrumentation(),
+      new ExpressInstrumentation(),
+      new NestInstrumentation(),
+      new AmqplibInstrumentation(),
+    ],
+  });
+
+  sdk.start();
+
+  process.on('SIGTERM', () => {
+    sdk
+      .shutdown()
+      .then(() => console.log('Tracing terminated'))
+      .catch((error) => console.log('Error terminating tracing', error))
+      .finally(() => process.exit(0));
+  });
+}
+
+module.exports = setupOpenTelemetry;
